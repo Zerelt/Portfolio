9fa0150e4c2df1c6af1f7b4d3378be9845ac959c
Dobre Alexandru <axel.xander1@gmail.com>
2020-03-06 02:37:42 +0100

    code format consistency, add new prettier rule

--- src/assets/theme/colors.js
+++ src/assets/theme/colors.js
@@ -2,9 +2,9 @@
 // change accent color to conform with google's contrast suggestions/requirements
 
 const COLORS = {
-  ACCENT : '#f3b100', //FFB2B2  f56a53 ef5d45
-  DARKBLACK : '#050505', //080808
-  WHITE : '#f9f9f9', //fafafa
+  ACCENT : '#f3b100', //  FFB2B2  f56a53 ef5d45
+  DARKBLACK : '#050505', // 080808
+  WHITE : '#f9f9f9', // fafafa
   BLACK : '#1a1a1a',
   GRAY : '#e8e8e8'
 }
