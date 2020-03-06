8b0b8b0cf34c126bc6b03e4dff58965546f64eb8
Dobre Alexandru <axel.xander1@gmail.com>
2020-03-06 01:56:35 +0100

    add netlify form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -72,7 +72,7 @@ class Contact extends Component {
           Say Hi<span>.</span>
         </ContactHeader>
         <ContactForm name='contact' netlify method='post'>
-          <input type='hidden' name='form-name' value='contact' />
+          <input type='hidden' name='contact' value='contact' />
           <InputContainer>
             <NameAndEmail>
               <NameBox nameFieldTop={nameFieldTop} height={height}>
