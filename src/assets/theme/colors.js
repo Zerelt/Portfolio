105cd0bbcceedf1e2781d53325a882951bbc1fb0
Alex Dobre <axel.xander1@gmail.com>
2020-03-07 14:41:33 +0100

    Improve contact section animation (#6)

--- src/assets/theme/colors.js
+++ src/assets/theme/colors.js
@@ -2,11 +2,11 @@
 // change accent color to conform with google's contrast suggestions/requirements
 
 const COLORS = {
-  ACCENT : '#f3b100', //  FFB2B2  f56a53 ef5d45
-  DARKBLACK : '#050505', // 080808
-  WHITE : '#f9f9f9', // fafafa
-  BLACK : '#1a1a1a',
-  GRAY : '#e8e8e8'
+  ACCENT: '#f3b100', //  FFB2B2  f56a53 ef5d45 FF5757
+  DARKBLACK: '#050505', // 080808 242E42
+  WHITE: '#f9f9f9', // fafafa
+  BLACK: '#1a1a1a',
+  GRAY: '#e8e8e8'
 }
 
 export default COLORS
