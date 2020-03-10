f76062460369155b31fcc3846cd8217ee49bd4df
Dobre Alexandru <axel.xander1@gmail.com>
2020-03-10 20:16:16 +0100

    Change accent color. Undo buttonBox transition timings

--- src/assets/theme/colors.js
+++ src/assets/theme/colors.js
@@ -1,10 +1,8 @@
 
-// change accent color to conform with google's contrast suggestions/requirements
-
 const COLORS = {
-  ACCENT: '#f3b100', //  FFB2B2  f56a53 ef5d45 FF5757 ff533c
+  ACCENT: '#FF5757', // f3b100 FFB2B2  f56a53 ef5d45 FF5757 ff533c
   DARKBLACK: '#050505', // 080808 242E42
-  WHITE: '#f9f9f9', // fafafa FEFBF6
+  WHITE: '#FEFBF6', // f9f9f9 fafafa FEFBF6
   BLACK: '#1a1a1a',
   GRAY: '#e8e8e8'
 }
